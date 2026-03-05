bfdbb4988e41eb83a7a4c2976ca9e7a7ee002289
radharadhajaap <cosmicpathastrology@gmail.com>
2026-03-05 10:04:51 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'jaap-pro-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
